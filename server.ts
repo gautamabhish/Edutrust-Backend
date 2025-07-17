14aefe841f6d6660a2dccead8124f3bc722396cc
gautamabhish <abhishekgautam.080470@gmail.com>
2025-07-17 22:10:27 +0530

    check 1 for devops test 2

--- server.ts
+++ server.ts
@@ -19,7 +19,7 @@ app.use(cors(
 ));
 app.use(express.json());
 app.get('/', (req, res) => {
-  res.send('Access Blocked for devops test ...');
+  res.send('Access Blocked for devops test 2 ...');
 });
 
 app.use('/api/users/auth',userRoutes);
